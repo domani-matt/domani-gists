afd6b59a9cea06290f5d37d66f0e992613e2758b
domani-matt <mwilcox@domanistudios.com>
2014-01-06 12:20:21 -0500

    home data attribute removed

--- inc/home.php
+++ inc/home.php
@@ -14,7 +14,7 @@ foreach($userlist as $u){
 		$out = "<h3>$info[message]</h3>\n";
 		break;
 	}
-	$out .= "	<li data-color=\"rgba(0,0,0,0.35)\"><a href=\"/$info[login]/\"><img src=\"$info[avatar_url]\" alt=\"$info[login]\" /><strong>$info[login]</strong>".(isset($info['name']) ? "<small>$info[name]</small>" : "")."</a></li>\n";
+	$out .= "	<li><a href=\"/$info[login]/\"><img src=\"$info[avatar_url]\" alt=\"$info[login]\" /><strong>$info[login]</strong>".(isset($info['name']) ? "<small>$info[name]</small>" : "")."</a></li>\n";
 }
 ?>
 <ul id="gistlist" class="unstyled clearfix">
